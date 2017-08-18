4cd426f8f3e44b4e5e26d362dab86af25a6d4468
milan.zivanov <milan.zivanov@gmail.com>
2017-08-18 16:59:02 +0200

    adding files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
